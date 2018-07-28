ace2dfe19726219e83b09f050bfb132ecbd46f6a
KrBaIO3 <krbaio3@gmail.com>
2018-07-28 16:43:17 +0200

    init

---
+++ src/app/angular-material/angular-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Hammer
+import 'hammerjs';
+
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [CommonModule, BrowserAnimationsModule],
+  declarations: []
+})
+export class AngularMaterialModule {}
