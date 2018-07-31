82f4b3a569227dc9d4834f62f9dab9a229393477
KrBaIO3 <krbaio3@gmail.com>
2018-07-31 18:13:27 +0200

    feat(ingresosGastos)
    Parte 2
    - Obtencion de uid de firebase
    - Obtencion de collection de firebase

--- src/app/ingresos-gastos/ingresos-gastos.service.ts
+++ src/app/ingresos-gastos/ingresos-gastos.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, DocumentReference } from 'angularfire2/firestore';
 import { IngresoGasto } from './models/ingreso-gasto.model';
 import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +12,41 @@ import { AuthService } from '../auth/auth.service';
 export class IngresosGastosService {
   constructor(
     private afDB: AngularFirestore,
-    public authService: AuthService
+    public authService: AuthService,
+    private store: Store<AppState>
   ) {}
 
-  crearIngresoGasto(
-    ingresoGasto: IngresoGasto
-  ): Promise<DocumentReference> {
+  initIngresoGastoListener() {
+    const user = this.authService.getUsuario();
+
+    console.log(user.uid);
+
+    this.store
+      .select('auth')
+      .pipe(
+        filter(auth => auth.user !== null)
+      )
+      .subscribe(auth => {
+        console.log(auth.user.uid);
+        this.ingresoGastoItem(auth.user.uid);
+      });
+  }
+
+  crearIngresoGasto(ingresoGasto: IngresoGasto): Promise<DocumentReference> {
     const user = this.authService.getUsuario();
 
     return this.afDB
       .doc(`${user.uid}/ingresos-gastos`)
       .collection('items')
       .add({ ...ingresoGasto });
+  }
+
+  private ingresoGastoItem ( uid: string ) {
+
+    this.afDB.collection(`${uid}/ingresos-gastos/items`)
+      .valueChanges()
+      .subscribe( docData => {
+        console.log(docData);
+      });
   }
 }
