78e212c8814c73981a1a62511636f618c22516b4
KrBaIO3 <krbaio3@gmail.com>
2018-08-02 02:59:14 +0200

    feat(usuario)
    - Logica para mostrar usuario en NavBar y SideBar
    - Pipe para Capitalizar
    - Fix Footer , iconos y nombres
    - Fix angular.json

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { User } from '../../auth/models/user.model';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  public nombre: string;
 
-  constructor() { }
+  public subscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.subscription = this.store.select('auth')
+      .pipe(
+        filter(auth => auth.user != null)
+      )
+      .subscribe(
+        auth => {
+          this.nombre = auth.user.nombre;
+          console.log(this.nombre);
+        }
+      );
   }
 
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+
+    this.subscription.unsubscribe();
+  }
 }
