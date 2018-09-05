cb75df6ba4d1d977c5816a9b8095f4f5ec602fc9
KrBaIO3 <krbaio3@gmail.com>
2018-09-05 21:05:38 +0200

    feat(material)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,24 +9,35 @@ import { RouterModule } from '@angular/router';
 
 // Componentes
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+import { NavBarComponent2 } from './nav-bar/nav-bar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreModule,
-    RouterModule
+    RouterModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   declarations: [
     FooterComponent,
-    NavbarComponent,
-    SidebarComponent
+    // NavbarComponent,
+    SidebarComponent,
+    NavBarComponent2
   ],
   exports: [
     FooterComponent,
-    NavbarComponent,
-    SidebarComponent
+    NavBarComponent2,
+    SidebarComponent,
+    // NavBarComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
